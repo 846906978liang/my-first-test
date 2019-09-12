9e0f07699eafe985329f45d3561ebe6e9da9f431
liang.hua <liang.hua@tendcloud.com>
2019-09-12 17:13:51 +0800

    发现问题

--- src/main/java/demo/Demo.java
+++ src/main/java/demo/Demo.java
@@ -1,5 +1,8 @@
 package demo;
 
+import com.sun.scenario.effect.impl.sw.java.JSWBlend_SRC_OUTPeer;
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 /**
  * @author hl
  * @version 1.0
@@ -7,14 +10,7 @@ package demo;
  */
 public class Demo {
 
-    public static void quickSort(){
-        System.out.println("一号玩家的内容是这个");
-    }
-    public static int partition(){
-        return 0;
-    }
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        System.out.println("我发现了，一个电脑用户无法模拟多人，说明版本控制还是有区分度的，一个人再怎么乱搞，自己的项目都不会出问题");
     }
-
 }
