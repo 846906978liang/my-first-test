9d01195b69491b015937b15348cd2181a8d936a0
liang.hua <liang.hua@tendcloud.com>
2019-09-12 16:57:15 +0800

    试试分支

---
+++ src/main/java/demo/Demo.java
@@ -0,0 +1,14 @@
+package demo;
+
+/**
+ * @author hl
+ * @version 1.0
+ * @date 2019/9/12 16:56
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+
+}
