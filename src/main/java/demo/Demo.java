f71a782649fd760330658b8113e90356605a7ebc
liang.hua <liang.hua@tendcloud.com>
2019-11-04 19:16:13 +0800

    久违的一次提交

--- src/main/java/demo/Demo.java
+++ src/main/java/demo/Demo.java
@@ -1,20 +1,12 @@
 package demo;
 
-import com.sun.scenario.effect.impl.sw.java.JSWBlend_SRC_OUTPeer;
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
-
 /**
  * @author hl
  * @version 1.0
  * @date 2019/9/12 16:56
  */
 public class Demo {
-
     public static void main(String[] args) {
-        System.out.println("一个电脑用户无法模拟多人，说明版本控制还是有区分度的，一个人再怎么乱搞，自己的项目都不会出问题");
-        System.out.println("你说多难啊，弄个冲突");
-        System.out.println("弄个冲突那么难吗");
-        System.out.println("必须要弄个冲突出来");
-        System.out.println("新的分支");
+
     }
 }
