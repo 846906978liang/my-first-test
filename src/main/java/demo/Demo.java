3da1b1d4a1fbd9febcbd7d0f80ce88b735de7ee9
liang.hua <liang.hua@tendcloud.com>
2019-11-05 11:10:16 +0800

    master完成了任务，要离开了

--- src/main/java/demo/Demo.java
+++ src/main/java/demo/Demo.java
@@ -17,5 +17,8 @@ public class Demo {
         System.out.println("弄个冲突那么难吗");
         System.out.println("必须要弄个冲突出来");
         System.out.println("人之初，性本善");
+        System.out.println("通过开启多个分支，模拟多用户，多用户，比如，dev分支pull and push origin/master"
+            + "内容与master 有着很大的不同，在master进行commit之后进行pull，出现冲突 ，merge操作");
+        System.out.println("感人的提交曲线");
     }
 }
